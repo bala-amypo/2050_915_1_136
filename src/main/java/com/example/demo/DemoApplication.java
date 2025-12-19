838574cb9349c2eed8158c68a133d9cb49acd43b
bala-amypo <bala@amypo.in>
2025-12-19 21:09:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication { // Change this from LoanEligibilityEmiRiskCheckerTest
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
