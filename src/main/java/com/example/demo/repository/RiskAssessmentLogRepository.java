1446503307562b5ffa5dac8263f1fcdcb596737d
bala-amypo <bala@amypo.in>
2025-12-19 09:56:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
+++ src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.RiskAssessment;
+import com.example.demo.entity.RiskAssessmentLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface RiskAssessmentLogRepository extends JpaRepository<RiskAssessment, Long> {
+public interface RiskAssessmentLogRepository extends JpaRepository<RiskAssessmentLog, Long> {
     Optional<RiskAssessment> findByLoanRequestId(Long loanRequestId);
 }
--- src/main/java/com/example/demo/service/RiskAssessmentService.java
+++ src/main/java/com/example/demo/service/RiskAssessmentService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.RiskAssessment;
+import com.example.demo.entity.RiskAssessmentLog;
 
 public interface RiskAssessmentService {
-    RiskAssessment assessRisk(Long loanRequestId);
-    RiskAssessment getByLoanRequestId(Long loanRequestId);
+    RiskAssessmentLog assessRisk(Long loanRequestId);
+    RiskAssessmentLog getByLoanRequestId(Long loanRequestId);
 }
