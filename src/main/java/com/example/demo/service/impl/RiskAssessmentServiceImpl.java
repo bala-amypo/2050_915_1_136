a60cb3e4b2d817d8bda91b2d63712d62e1d5a68c
bala-amypo <bala@amypo.in>
2025-12-20 18:35:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
@@ -1,10 +1,10 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.RiskAssessmentLog;
+import com.example.demo.entity.RiskAssessment;
 import com.example.demo.entity.LoanRequest;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.LoanRequestRepository;
-import com.example.demo.repository.RiskAssessmentLogRepository;
+import com.example.demo.repository.RiskAssessmentRepository;
 import com.example.demo.service.RiskAssessmentService;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +12,20 @@ import org.springframework.stereotype.Service;
 public class RiskAssessmentServiceImpl implements RiskAssessmentService {
 
     private final LoanRequestRepository loanRepo;
-    private final RiskAssessmentLogRepository riskRepo;
+    private final RiskAssessmentRepository riskRepo;
 
     public RiskAssessmentServiceImpl(LoanRequestRepository loanRepo,
-                                     RiskAssessmentLogRepository riskRepo) {
+                                     RiskAssessmentRepository riskRepo) {
         this.loanRepo = loanRepo;
         this.riskRepo = riskRepo;
     }
 
     @Override
-    public RiskAssessmentLog assessRisk(Long loanRequestId) {
+    public RiskAssessment assessRisk(Long loanRequestId) {
         LoanRequest lr = loanRepo.findById(loanRequestId)
                 .orElseThrow(() -> new ResourceNotFoundException("Loan request not found"));
 
-        RiskAssessmentLog log = new RiskAssessmentLog();
+        RiskAssessment log = new RiskAssessment();
         log.setLoanRequest(lr);
         log.setRiskScore(60.0);
         log.setRiskLevel("MEDIUM");
