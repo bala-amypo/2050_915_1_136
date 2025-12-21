3f07f00e154148612cd35b8cc8e2f7bf082ee26c
bala-amypo <bala@amypo.in>
2025-12-21 17:33:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoanRequestRepository.java
+++ src/main/java/com/example/demo/repository/LoanRequestRepository.java
@@ -2,12 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.LoanRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-@Repository
 public interface LoanRequestRepository extends JpaRepository<LoanRequest, Long> {
 
-    // Notice the underscore: findByUser_Id
-    List<LoanRequest> findByUser_Id(Long userId);
+    // ✅ ADD THIS METHOD
+    List<LoanRequest> findByUserId(Long userId);
 }
