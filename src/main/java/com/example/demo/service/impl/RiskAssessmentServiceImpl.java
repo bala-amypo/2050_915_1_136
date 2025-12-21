ef3484a2a5e2f5ba21d1cc645b7ba226925ed9c9
bala-amypo <bala@amypo.in>
2025-12-21 22:44:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentServiceImpl.java
@@ -1,9 +1,14 @@
+package com.example.demo.service.impl;
+
 import com.example.demo.entity.RiskAssessment;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.RiskAssessmentRepository;
 import com.example.demo.service.RiskAssessmentService;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
+@Service
 public class RiskAssessmentServiceImpl implements RiskAssessmentService {
 
     private final RiskAssessmentRepository repository;
@@ -13,7 +18,14 @@ public class RiskAssessmentServiceImpl implements RiskAssessmentService {
     }
 
     @Override
-    public Optional<RiskAssessment> getByLoanRequestId(Long loanRequestId) {
-        return repository.findByLoanRequestId(loanRequestId);
+    public List<RiskAssessment> getByLoanRequestId(Long loanRequestId) {
+        List<RiskAssessment> list = repository.findByLoanRequestId(loanRequestId);
+
+        if (list.isEmpty()) {
+            throw new ResourceNotFoundException(
+                    "No Risk Assessment found for LoanRequest ID: " + loanRequestId
+            );
+        }
+        return list;
     }
 }
